a80418f3ac0f4a4cad37f4c4eb9da245e98c9f3c
zhouyue <zy514520>
2018-09-20 22:58:50 +0800

    项目第一次提交

---
+++ gs-test/src/main/java/com/own/gstest/GsTestApplication.java
@@ -0,0 +1,12 @@
+package com.own.gstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsTestApplication.class, args);
+	}
+}
