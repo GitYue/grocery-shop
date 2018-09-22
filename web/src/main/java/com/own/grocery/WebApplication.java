f3fafcb9c12e5436fa3c2643d28a89ff5628edbe
zhouyue <zy514520>
2018-09-22 22:37:18 +0800

    多数据源配置，完成

---
+++ web/src/main/java/com/own/grocery/WebApplication.java
@@ -0,0 +1,14 @@
+package com.own.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+}
