893fe077541f3192904319545f6bfed958295792
zhouyue <zy514520>
2018-11-12 23:22:40 +0800

    删除无用代码

--- web/src/main/java/com/own/grocery/config/test/TestDbConfig.java
+++ web/src/main/java/com/own/grocery/config/test/TestDbConfig.java
@@ -2,7 +2,6 @@ package com.own.grocery.config.test;
 
 import com.own.grocery.config.common.BaseDbConfig;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -10,15 +9,9 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @MapperScan(basePackages = {"com.own.grocery.center.test.repository"},sqlSessionFactoryRef = "testSqlSessionFactory")
--- web/src/main/java/com/own/grocery/config/test2/Test2DbConfig.java
+++ web/src/main/java/com/own/grocery/config/test2/Test2DbConfig.java
@@ -2,22 +2,15 @@ package com.own.grocery.config.test2;
 
 import com.own.grocery.config.common.BaseDbConfig;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @MapperScan(basePackages = {"com.own.grocery.center.test2.repository"},sqlSessionFactoryRef = "test2SqlSessionFactory")
